f920490d24dbb510e2c87312bdb49fb173499d7a
Nzadrozna <nickzadrozna93@gmail.com>
2019-01-31 11:07:12 -0800

    2more done

--- labs/02_basic_datatypes/02_01_cylinder.py
+++ labs/02_basic_datatypes/02_01_cylinder.py
@@ -4,3 +4,10 @@ of a cylinder with a radius of 3.14 and a height of 5. Print out the result.
 
 
 '''
+import math
+
+print (math.pi*5**2*3.14)
+#volume
+
+print(2*math.pi*3.14*5+2*math.pi*3.14**2)
+#surface area

e07806aef039027a3da84a638bbdaa5260028ce9
Nzadrozna <nickzadrozna93@gmail.com>
2019-02-04 14:35:53 -0800

    here its done

--- labs/02_basic_datatypes/02_02_days_seconds.py
+++ labs/02_basic_datatypes/02_02_days_seconds.py
@@ -6,8 +6,9 @@ NOTE: We will use the input() funtion to collect users input. An example is demo
 
 '''
 
-# the input of the user will be saved in the variable days.
+# the input of the user will be saved in the variable days. (sentence)
 # because the input() function collects the input as a string, we have to convert it to an int
 # The string passed to the input() function is what the user is prompted with
 days = int(input("Please enter a number in days between 1 and 1,000,000,000: "))
 
+sentence = input("Pick a random number of days from 1 to 1,0000,0000")
